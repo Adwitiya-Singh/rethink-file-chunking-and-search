189523ebe9026625f5ff9089e3dce05f5ca35287
Adwitiya Singh <adwitiya.karan@gmail.com>
2021-02-07 20:19:41 -0800

    Rewrite server to use database

--- server.js
+++ server.js
@@ -3,23 +3,32 @@ const app = express()
 const port = 3000
 const parse   = require('csv-parse');
 const fs = require('fs')
+const sqlite3 = require('sqlite3').verbose();
+let db = new sqlite3.Database('Database/test.db', (err) => {
+    if (err) {
+        return console.error(err.message);
+    }
+    console.log('Connected to the  SQlite database.');
+});
+
 
 app.get('/', (req, res) => {
+    let place = (req.query.page?req.query.page:1)*100;
 
-    let csvData=[];
+    if (req.query.search){
+        sql = `SELECT * FROM data WHERE '${req.query.search}' IN (Region,Country,Item_Type,Sales_Channel,Order_Priority,Order_Date,Order_ID,Ship_Date,Units_Sold,Unit_Price,Unit_Cost,Total_Revenue,Total_Cost,Total_Profit) limit 100 offset ${place}`;
+    }else {
 
-    let size = (req.query.page?req.query.page:1)*100;
+        sql = `SELECT * FROM data limit 100 offset ${place}`;
+    }
 
-    fs.createReadStream("records.csv")
-        .pipe(parse({delimiter: ',', from:size, to_line:size+100}))
-        .on('data', function(csvrow) {
-           size+=100;
-            csvData.push(csvrow);
-        })
-        .on('end',function() {
-            //do something with csvData
-            res.send(csvData)
-        });
+    db.all(sql, [], (err, rows) => {
+        if (err) {
+            throw err;
+        }
+        res.send(rows);
+    });
+
 })
 
 
