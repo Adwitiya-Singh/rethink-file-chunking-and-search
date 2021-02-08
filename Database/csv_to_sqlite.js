6a6d1f43987f015cd2f0d422a1775833f60c1f0e
Adwitiya Singh <adwitiya.karan@gmail.com>
2021-02-07 20:18:55 -0800

    Restructure

--- Database/csv_to_sqlite.js
+++ Database/csv_to_sqlite.js
@@ -1,13 +1,35 @@
-const CSV2SQL = require('csv2sql-lite');
+const sqlite3 = require('sqlite3').verbose();
+const fs = require('fs')
+const parse   = require('csv-parse');
 
-
-var fs = require('fs');
-var rstream = fs.createReadStream('./records.csv');
-var wstream = fs.createWriteStream('./mysql.sql');
-
-var csv2sql = CSV2SQL({
-    tableName: 'test',
-    dbName: 'testDb',
+let db = new sqlite3.Database('test.db', (err) => {
+    if (err) {
+        return console.error(err.message);
+    }
+    console.log('Connected to the  SQlite database.');
 });
 
-rstream.pipe(csv2sql).pipe(wstream);
+let createTable = () => {
+    fs.createReadStream("records.csv")
+        .pipe(parse({delimiter: ',', to_line: 1}))
+        .on('data', function (csvrow) {
+            db.run(`CREATE TABLE data((?) text,(?) text,(?) text,(?) text,(?) text,(?) text,(?) text,(?) text,(?) text,(?) text,(?) text,(?) text,(?) text,(?) text)`, csvrow);
+        })
+        .on('end', function () {
+            db.close();
+        });
+ db.run('CREATE TABLE data(Region text,Country text,Item_Type text,Sales_Channel text,Order_Priority text,Order_Date text,Order_ID text,Ship_Date text,Units_Sold text,Unit_Price text,Unit_Cost text,Total_Revenue text,Total_Cost text,Total_Profit text)');
+}
+let addValues = () => {
+    fs.createReadStream("records.csv")
+        .pipe(parse({delimiter: ',', from: 2}))
+        .on('data', function (csvrow) {
+            db.run(`INSERT INTO data(Region,Country,Item_Type,Sales_Channel,Order_Priority,Order_Date,Order_ID,Ship_Date,Units_Sold,Unit_Price,Unit_Cost,Total_Revenue,Total_Cost,Total_Profit) VALUES((?), (?), (?), (?), (?), (?), (?), (?), (?), (?), (?), (?), (?), (?))`, csvrow);
+        })
+        .on('end', function () {
+            db.close();
+        });
+}
+
+createTable()
+addValues()
